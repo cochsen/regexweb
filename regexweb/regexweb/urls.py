abf4be9d719f80241f06fc855a5b30d196358c78
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2016-08-14 18:48:13 -0400

    added views and templates

--- regexweb/regexweb/urls.py
+++ regexweb/regexweb/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.core.urlresolvers import reverse_lazy
+from entries.views import HomeView, EntryListView, EntryFormView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^entries/$', EntryListView.as_view(), name = 'entries'),
+    url(r'^entries/insert$', EntryFormView.as_view(), name = 'insert')
+    url(r'^login/$', 
+        auth_views.login, 
+        kwargs = {'template_name': 'admin/login.html'},
+        name = 'login'),
+    url(r'^logout/$',
+        auth_views.logout,
+        kwargs = {'next_page': reverse_lazy('home')},
+        name = 'logout'),
+    url(r'^$', HomeView.as_view(), name = 'home')
 ]
